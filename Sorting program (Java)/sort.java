7ad3676e5dbc4ee67922e7a2e3b75af81bb7fabd
Abdulla Moledina <abdullahalimoledina@gmail.com>
2022-06-25 15:06:15 +0100

    Java version fully implemented

--- Sorting program (Java)/sort.java
+++ Sorting program (Java)/sort.java
@@ -1,76 +1,58 @@
 import java.util.*;
 
 public class sort {
-
-    //swap function
-    public static void swap (int a, int b) {
-        int temp = a;
-        a = b;
-        b = temp;
-    }
     
     //bubble sort
-    public static int bubble (int[] list, int length) {
-        //run variable to allow the algorithm to keep going through the list, until it goes through the list with no swaps
-        boolean run = true;
-        while (run) {
-            //increments whenever a swap is made
-            int count = 0;
-            //runs through the list
-            for (int i = 0; i < length; i++) {
-                //swap if the next value is greater than the current one
-                if (list[i] > list[i+1]) {
-                    swap(list[i], list[i+1]);
-
-                    //increment count
-                    count += 1;
+    public static int bubble (int list[], int length) {
+        //runs through the list
+        for (int i = 0; i < length - 1; i++) {
+            //checks if a swap has occurred
+            boolean swapped = false;
+            //runs through the list to compare each value to the next one
+            for (int j = 0; j < length - i - 1; j++) {
+                //if the current value is greater than the next one, swap them
+                if (list[j] > list[j+1]) {
+                    int temp = list[j];
+                    list[j] = list[j+1];
+                    list[j+1] = temp;
+                    //set swapped to true
+                    swapped = true;
                 }
             }
-            //if count is 0, then no swaps have been made, meaning algorithm is complete
-            if (count == 0) {
-                run = false;
+            
+            //if no swaps have occurred, then the algorithm is complete
+            if (!swapped) {
+                break;
             }
         }
 
         //bubble sort successful
-        System.out.println("BUBBLED");
         return 0;
     }
 
     //insertion sort
-    public static int insertion (int[] list, int length) {
-        //new list to store the sorted numbers
-        int[] sorted = new int[length];
-        //stores the first variable of the list into the sorted list
-        sorted[0] = list[0];
-        //runs through the list
-        for (int i = 1; i < length; i++) {
-            //stores the next variable of the list into the sorted list
-            sorted[i] = list[i];
-            //count variable to change position of data in sorted list
-            int count = i;
-
-            //runs through the sorted list
-            for (int j = 1; j <= i; j++) {
-                //swap values
-                if (sorted[count] < sorted[i-j]) {
-                    swap(sorted[i-j], sorted[count]);
-                    //decrease count
-                    count -= 1;
-                //no swap needed
-                } else {
-                    break;
-                }
+    public static int insertion (int list[], int length) {
+        //runs through the list starting at index 1
+        for (int index = 1; index < length; index++) {
+            //stores the value to be compared
+            int value = list[index];
+            //stores the index of the value to be compared
+            int j = index - 1;
+            //compare value with each element on the leftside until it finds an element smaller than it
+            while (j >= 0 && value < list[j]) {
+                list[j + 1] = list[j];
+                j--;
             }
+            //insert the value into the correct position
+            list[j + 1] = value;
         }
 
         //insertion sort successful
-        System.out.println("INSERTED");
         return 0;
     }
 
     //sorting function for merge sort
-    public static void mergeSort (int[] list, int start, int middle, int end) {
+    public static void mergeSort (int list[], int start, int middle, int end) {
         //calculate length of lists to be put together
         int length1 = middle - start + 1;
         int length2 = end - middle;
@@ -125,7 +107,7 @@ public class sort {
     }
 
     //merge sort
-    public static int merge (int[] list, int start, int end) {
+    public static int merge (int list[], int start, int end) {
 
         //if the list is only one element long, it is already sorted
         if (start < end) {
@@ -133,20 +115,19 @@ public class sort {
             int middle = (start + end) / 2;
 
             //divie the list into smaller lists recursively
-            merge(list, start, end);
-            merge(list, middle + 1, end);
+            sort.merge(list, start, middle);
+            sort.merge(list, middle + 1, end);
             
             //merge the two halves
-            mergeSort(list, start, middle, end);
+            sort.mergeSort(list, start, middle, end);
         }
 
         //merge sort successful
-        System.out.println("MERGED");
         return 0;
     }
 
     //function to put pivot element in the right place (partitioning the list)
-    public static int partition (int[] list, int left, int right) {
+    public static int partition (int list[], int left, int right) {
 
         //pivot element is the rightmost element
         int pivot = list[right];
@@ -157,34 +138,37 @@ public class sort {
         //runs through the list, and compares each element to the pivot element
         for (int j = left; j < right; j++) {
             //if the element is smaller than the pivot element, move it to the left
-            if (list[j] < pivot) {
+            if (list[j] <= pivot) {
                 i++;
-                swap(list[i], list[j]);
+                int temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
             }
         }
 
         //swap the pivot element with the element at the rightmost position
-        swap(list[i+1], list[right]);
+        int temp = list[i + 1];
+        list[i + 1] = list[right];
+        list[right] = temp;
 
         //return the index of the pivot element
         return (i+1);
     }
     
     //quick sort
-    public static int quick (int[] list, int start, int end) {
+    public static int quick (int list[], int start, int end) {
 
         //if the list is only one element long, it is already sorted
         if (start < end) {
             //find the pivot element and put all elements < pivot on the left, and all elements > pivot on the right
-            int pivot = partition(list, start, end);
+            int pivot = sort.partition(list, start, end);
             
             //call the left and right halves of the list recursively
-            quick(list, start, pivot - 1);
-            quick(list, pivot + 1, end);
+            sort.quick(list, start, pivot - 1);
+            sort.quick(list, pivot + 1, end);
         }
 
         //quick sort successful
-        System.out.println("QUICK");
         return 0;
     }
     
@@ -204,10 +188,7 @@ public class sort {
         System.out.print("Enter the list you want sorted: ");
         for (int i = 0; i < length; i++) {
             list[i] = stdin.nextInt();
-            //if user puts less elements than length
         }
-
-
 
         //choose which sorting algorithm to use
         int choice;
@@ -224,14 +205,23 @@ public class sort {
             System.out.println("Choice out of bounds");
             System.exit(1);
         } else if (choice == 1) {
-            bubble(list, length);
+            sort.bubble(list, length);
         } else if (choice == 2) {
-            insertion(list, length);
+            sort.insertion(list, length);
         } else if (choice == 3) {
-            merge(list, 0, length - 1);
+            sort.merge(list, 0, length - 1);
         } else {
-            quick (list, 0, length - 1);
+            sort.quick (list, 0, length - 1);
         }
+
+        //print sorted list
+        System.out.print("Sorted list: ");
+        for (int i = 0; i < length; i++) {
+            System.out.print(list[i] + " ");
+        }
+        System.out.println("");
+
+        stdin.close();
 
         System.out.println("SORTED");
     }
